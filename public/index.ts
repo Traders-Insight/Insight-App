d070a009f3c2ae1910a792e793b50f1c405758a7
ayo <0xosepatrick@gmail.com>
2025-04-02 20:43:52 +0100

    add images

---
+++ public/index.ts
@@ -0,0 +1,6 @@
+import SignUpImg from "./images/signup-Img.png";
+import LoginImg from "./images/login-img.png";
+import ForgotPasswordImage from "./images/forgot-password-image.png";
+import Logo from "./svg/logo.svg";
+
+export { SignUpImg, Logo, LoginImg, ForgotPasswordImage };
